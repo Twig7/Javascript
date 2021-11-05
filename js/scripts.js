3cb52a61cb8459bf9053c0a922953df04d19691a
Amber W <amoothenielson@gmail.com>
2021-11-05 13:38:03 -0700

    Add answers of quiz, and give them center alignment. Add js variables for the questions. Hope for the best.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
 $(document).ready(function() {
   }
+
+const cryptid=$("#cryptid").val();
+const time=$("#time").val();
+const monster=$("#monster").val();
+const smell=$("#smell").val();
+const food=$("#food").val();
+const pets=$("#pets").val();
+
+
+
+$(cryptid).val();
+$(time).val();
+$(monster).val();
+$(smell).val();
+$(food).val();
+$(pets).val();
