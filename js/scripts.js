3e4f304bf8e5ae15d566959f2bb92caee57efad5
Amber W <amoothenielson@gmail.com>
2021-11-06 14:17:50 -0700

    put entire scripts code into note form to reference for later.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
+//$(document).ready(function() {
   
-  let cryptid=$("#cryptid").val();
-  let time=$("#time").val();
-  let monster=$("#monster").val();
-  let smell=$("#smell").val();
-  let food=$("#food").val();
-  let pets=$("#pets").val();
+  //let cryptid=$("#cryptid").val();
+  //let time=$("#time").val();
+  //let monster=$("#monster").val();
+  //let smell=$("#smell").val();
+  //let food=$("#food").val();
+  //let pets=$("#pets").val();
 
-  let cryptid = parseInt(cryptid);
-  let time = parseInt(time);
-  let monster = parseInt(monster);
-  let smell = parseInt(smell);
-  let food = parseInt(food);
-  let pets = parseInt(pets);
+  //let cryptid = parseInt(cryptid);
+  //let time = parseInt(time);
+  //let monster = parseInt(monster);
+  //let smell = parseInt(smell);
+  //let food = parseInt(food);
+  //let pets = parseInt(pets);
 
-  function add (cryptid, time, monster, smell, food, pets) {
-    return cryptid + time + monster + smell + food + pets;
-  }
-  const result = (cryptid + time + monster + smell + food + pets);
-});
+  //function add (cryptid, time, monster, smell, food, pets) {
+    //return cryptid + time + monster + smell + food + pets;
+  //}
+  //const result = (cryptid + time + monster + smell + food + pets);
+//});
