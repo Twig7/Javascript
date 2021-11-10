85c6b00b23460a7dbe828f9089ef7ec09d7019d1
Amber W <amoothenielson@gmail.com>
2021-11-09 20:37:35 -0800

    Switch result if else statement to inside of submit and ready function brackets per instructor suggestion.

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,23 @@ $(document).ready(function() {
     let pets=parseInt($("#pets").val()); 
     let result = (cryptid + time + monster + smell + food + pets);
     console.log(result);
+    if (result > 15) {
+      $("#result1").show();
+    } else if (result <= 10) {
+      $("#result2").show();
+    } else {
+      $("#result3").show();
+    }
   });
 });
   function add (cryptid, time, monster, smell, food, pets) {
     return cryptid + time + monster + smell + food + pets;
   }
 
-if (result > 15) {
-  $('#result1').show();
-} else if (result <= 10) {
-  $('#result2').show();
-} else {
-  $('#result3').show();
-}
+// if (result > 15) {
+//   $('#result1').show();
+// } else if (result <= 10) {
+//   $('#result2').show();
+// } else {
+//   $('#result3').show();
+// }
